bc277ceeaaf04f6c409feafc0a09603aa9a56495
Anusha Korada <110827588+Anusha03-25@users.noreply.github.com>
2025-07-27 18:40:13 +0530

    Update train_model.py

--- employee salary predection/train_model.py
+++ employee salary predection/train_model.py
@@ -5,26 +5,16 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 import joblib
 
-# Load dataset
-data = pd.read_csv("employee_data.csv")
+data=pd.read_csv("/content/employee_data_large.csv")
 
 X = data.drop("salary", axis=1)
 y = data["salary"]
 
-# Preprocessing: One-hot encoding for categorical features
-preprocessor = ColumnTransformer([
-    ("encoder", OneHotEncoder(), ["education", "role"])
-], remainder="passthrough")
+preprocessor = ColumnTransformer([("encoder", OneHotEncoder(), ["education", "role"])], remainder="passthrough")
 
-# Pipeline
-model = Pipeline([
-    ("preprocessing", preprocessor),
-    ("regressor", LinearRegression())
-])
+model = Pipeline([("preprocessing", preprocessor),("regressor", LinearRegression())])
 
-# Train
 model.fit(X, y)
 
-# Save
 joblib.dump(model, "model.pkl")
 print("Model trained and saved as model.pkl")
